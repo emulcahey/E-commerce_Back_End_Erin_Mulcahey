43256062f3ca1b58e26695a2aa48cc7b1fb9beb6
emulcahey <e.yehaclum@gmail.com>
2024-07-11 16:23:35 -0500

    fixed tags get all and tags get one code in routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,33 +1,42 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
+const express = require('express');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  Tag.findAll({
-    include: {
-      model: Product,
-      through: ProductTag,
-      as: 'tagged_product'
-    }
-  })
+  try {
+    const tags = await Tag.findAll({
+      include: {
+        model: Product,
+        through: ProductTag,
+      }
+      })
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json({ err: 'Failed to retrieve tags'});
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-  Tag.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: {
-      model: Product,
-      through: ProductTag,
-      as: 'tagged_product'
-    }
-  })
+  try {
+    const tags = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: {
+        model: Product,
+        through: ProductTag,
+      }
+    })
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json({ err: 'Failed to retrieve tag'});
+  }
 });
 
 router.post('/', (req, res) => {
