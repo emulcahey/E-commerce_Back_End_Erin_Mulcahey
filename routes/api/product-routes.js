4af42eda2b4002440a220c7faf18a7cdd2dbc842
emulcahey <e.yehaclum@gmail.com>
2024-07-11 16:38:59 -0500

    fixed products get one and get all code in routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,38 +4,46 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Product.findAll({
-    include: [
-      Category,
-      {
-        model: Tag,
-        through: ProductTag,
-        as: 'tagged_product'
-      }
-    ]
-  })
+  try {
+    const products = await Product.findAll({
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        }
+      ]
+    })
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ err: 'Failed to retrieve products'});
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  product.findOne({
-    where: {
-      id: req.params.id
-    },
-    include: [
-      Category,
-      {
-        model: Tag,
-        through: ProductTag,
-        as: 'tagged_product'
-      }
-    ]
-  })
+  try {
+    const products = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        }
+      ]
+    })
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ err: 'Failed to retrieve product'});
+  }
 });
 
 // create new product
